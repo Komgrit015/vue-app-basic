372da846b19cba83c457c7b76d4945de94728eac
Komgrit015 <ford_za007@hotmail.com>
2023-02-01 11:09:41 +0700

    REST API

--- src/components/MyBar.vue
+++ src/components/MyBar.vue
@@ -1,6 +1,10 @@
 <template lang="">
     <div>
-        
+        <ul>
+            <li><a href="/">My App</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">About</a></li>
+        </ul>
     </div>
 </template>
 <script>
@@ -8,6 +12,22 @@ export default {
     
 }
 </script>
-<style lang="">
-    
+<style scoped>
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+    }
+    li {
+        float: left;
+    }
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
 </style>
